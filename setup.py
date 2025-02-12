34e18c645ec05047d7cbdbe6cc8b9785e74960e1
Evelyn <yslaimofa@gmail.com>
2025-02-13 06:09:57 +0800

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical_ChatBot_AI',
+    version= '0.0.0',
+    author= 'Evelyn Lai',
+    author_email= 'evelynlaimofa@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
